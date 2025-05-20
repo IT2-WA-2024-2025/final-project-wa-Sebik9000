0f5664a0c2b40068fea10756fbb1d6ee113b4dc4
Gargosch <98969@student.vitkovickastredni.cz>
2025-05-20 02:48:31 +0200

    work in progress otázky

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/otazka', methods=['GET', 'POST'])
+def otazka():
+    question = None
+    if request.method == 'POST':
+        question = request.form['question']
+    return render_template('question.html', question=question)
 
 if __name__ == '__main__':
     app.run(debug=True)
