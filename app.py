c71cc767dc3567a4e16466c7a9f7fc86da9cdf0d
Gargosch <98969@student.vitkovickastredni.cz>
2025-05-23 01:01:16 +0200

    Predelani na databaze

--- app.py
+++ app.py
@@ -1,41 +1,57 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ankety.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# seznam pro ukládání otázek
-otazky = []
-# seznam pro ukládání odpovědí (každá položka je seznam odpovědí k jedné otázce)
-odpovedi = []
+class Otazka(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    odpovedi = db.relationship('Odpoved', backref='otazka', lazy=True)
+
+class Odpoved(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    otazka_id = db.Column(db.Integer, db.ForeignKey('otazka.id'), nullable=False)
+
+# Vytvoření databáze (při prvním spuštění)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('index.html', otazky=otazky, odpovedi=odpovedi)
+    otazky = Otazka.query.all()
+    return render_template('index.html', otazky=otazky)
 
 @app.route('/otazka', methods=['GET', 'POST'])
 def otazka():
     question = None
     if request.method == 'POST':
         question = request.form['question']
-        otazky.append(question)  # Přidá otázku do seznamu
-        odpovedi.append([])      # Přidá prázdný seznam odpovědí pro novou otázku
+        nova_otazka = Otazka(text=question)
+        db.session.add(nova_otazka)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('question.html', question=question)
 
 @app.route('/odpoved/<int:id>', methods=['GET', 'POST'])
 def odpoved(id):
+    otazka = Otazka.query.get_or_404(id)
     odpoved_text = None
-    if 0 <= id < len(otazky):
-        otazka = otazky[id]
-        if request.method == 'POST':
-            odpoved_text = request.form['odpoved']
-            odpovedi[id].append(odpoved_text)  # Uloží odpověď k otázce
-        return render_template('odpoved.html', otazka=otazka, odpoved=odpoved_text, vsechny_odpovedi=odpovedi[id])
-    else:
-        return render_template('odpoved.html', otazka=None, odpoved=None, vsechny_odpovedi=[])
-        
+    if request.method == 'POST':
+        odpoved_text = request.form['odpoved']
+        nova_odpoved = Odpoved(text=odpoved_text, otazka_id=otazka.id)
+        db.session.add(nova_odpoved)
+        db.session.commit()
+    vsechny_odpovedi = Odpoved.query.filter_by(otazka_id=otazka.id).all()
+    return render_template('odpoved.html', otazka=otazka.text, odpoved=odpoved_text, vsechny_odpovedi=[o.text for o in vsechny_odpovedi])
+
 @app.route('/statistika')
 def statistika():
-    pocet_otazek = len(otazky)
-    pocet_odpovedi = sum(len(o) for o in odpovedi)
+    pocet_otazek = Otazka.query.count()
+    pocet_odpovedi = Odpoved.query.count()
     return render_template('statistika.html', pocet_otazek=pocet_otazek, pocet_odpovedi=pocet_odpovedi)
 
 if __name__ == '__main__':
